0eeae1101eb007bb0dbad74cca76aa18fc7920fe
Krshnanando-Biplob <krsnanandobiplob@gmail.com>
2024-02-17 15:36:16 +0600

    date and time

--- dom.js
+++ dom.js
@@ -47,3 +47,20 @@ function squereRoot(){
 
 }
   
+function updateDateTime() {
+  const now = new Date();
+
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  const formattedDate = now.toLocaleDateString('en-US', options);
+
+  const formattedTime = now.toLocaleTimeString('en-US');
+
+  document.getElementById('date').textContent = ` ${formattedDate}`;
+  document.getElementById('time').textContent = `time: ${formattedTime}`;
+}
+
+// Update date and time every second
+setInterval(updateDateTime, 1000);
+
+// Initial update
+updateDateTime();
